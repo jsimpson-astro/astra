65ce9b55022804870c616e370c8249bd45ee3aca
Jordan <jordansimpson613@gmail.com>
2024-11-19 16:19:08 +0100

    cleanup

--- utils/utils.py
+++ utils/utils.py
@@ -3,13 +3,14 @@ __all__ = [
     'taper_spectrum',
     'calculate_vpx',
     'mask_interp'
-    ]
+]
 
 import numpy as np
 
 c_ = 299792.458
 
-def apply_mask(wvs: np.ndarray, mask_bounds: list) -> np.ndarray:
+
+def apply_mask(wvs: np.ndarray, mask_bounds: list[tuple[float, float]]) -> np.ndarray[bool]:
     """
     Apply a list of mask bounds, containing tuples of upper and lower bounds,
     to an array of wavelengths, returning a boolean mask of the same size.
@@ -18,13 +19,13 @@ def apply_mask(wvs: np.ndarray, mask_bounds: list) -> np.ndarray:
     wvs: np.ndarray
         Wavelength array to mask
     mask_bounds: list of tuples of 2 floats
-        List of tuples where each tuple is an upper and lower bound to 
+        List of tuples where each tuple is an upper and lower bound to
         exclude. The order does not matter.
 
     Returns:
     mask: np.ndarray
         Boolean array, same size as wvs. False where excluded by mask
-    
+
     """
 
     mask = np.ones(wvs.size, dtype=bool)
@@ -39,10 +40,10 @@ def apply_mask(wvs: np.ndarray, mask_bounds: list) -> np.ndarray:
 
 
 def taper_spectrum(
-    spectrum: np.ndarray, 
-    taper: float, 
+    spectrum: np.ndarray,
+    taper: float,
     taper_errors: bool = False,
-    ) -> np.ndarray:
+) -> np.ndarray:
     """
     Taper the ends of a spectrum using a cosine envelope.
     `taper` is the fraction of the spectrum to taper, from 0 to 1.
@@ -67,8 +68,8 @@ def taper_spectrum(
     idx1 = len(flux) - 1
 
     # identify ends to slice
-    slice0 = slice(idx0, idx0 + int(taper*flux.size) + 1)
-    slice1 = slice(idx1 - int(taper*flux.size), idx1 + 1)
+    slice0 = slice(idx0, idx0 + int(taper * flux.size) + 1)
+    slice1 = slice(idx1 - int(taper * flux.size), idx1 + 1)
 
     # compute factors to multiply by based on (index - end pixel)
     idxs = np.arange(flux.size)
@@ -94,13 +95,13 @@ def calculate_vpx(wvs: np.ndarray) -> float:
     Calculate the average velocity per pixel from an array of wavelengths.
 
     """
-    return c_ * (np.exp(np.log(wvs.max()/wvs.min()) / (wvs.size-1) ) - 1)
+    return c_ * (np.exp(np.log(wvs.max() / wvs.min()) / (wvs.size - 1)) - 1)
 
 
 def _mask_interp(
-    flux: np.ndarray[float], 
+    flux: np.ndarray[float],
     mask: np.ndarray[bool]
-    ) -> np.ndarray[float]:
+) -> np.ndarray[float]:
     """
     Linearly interpolate flux from edges of given mask.
     Masked edges are set to the nearest unmasked value.
@@ -114,10 +115,10 @@ def _mask_interp(
     Returns:
     flux_i: np.ndarray
         Fluxes of spectrum with interpolation over masked regions, same size as flux.
-    
+
     """
     flux_i = flux.copy()
-    
+
     # indices included in mask - so slice i:f+1
     mask_diff = np.diff(mask.astype(np.int8))
     mask_is = (np.r_[0, mask_diff] == -1).nonzero()[0]
@@ -128,37 +129,37 @@ def _mask_interp(
     if mask_is.size != 0 and mask_fs.size != 0:
         if mask_is[0] < mask_fs[0]:
             # left edge is not masked, go i[0]:f[0], i[1]:f[1]
-            slices = [slice(i, f+1) for i, f in zip(mask_is[:-1], mask_fs)]
+            slices = [slice(i, f + 1) for i, f in zip(mask_is[:-1], mask_fs)]
         else:
             # left edge is masked, go :f[0], i[0]:f[1] etc.
-            slices = [slice(None, mask_fs[0]+1)]
-            slices += [slice(i, f+1) for i, f in zip(mask_is[:-1], mask_fs[1:])]
-        
+            slices = [slice(None, mask_fs[0] + 1)]
+            slices += [slice(i, f + 1) for i, f in zip(mask_is[:-1], mask_fs[1:])]
+
         if mask_is[-1] < mask_fs[-1]:
             # right edge is not masked, i[-1]:f[-1] should be fine
-            slices += [slice(mask_is[-1], mask_fs[-1]+1)]
+            slices += [slice(mask_is[-1], mask_fs[-1] + 1)]
         else:
             # right edge is masked, will have to use i[-1]:
             slices += [slice(mask_is[-1], None)]
     elif mask_is.size != mask_fs.size:
-        slices = [slice(mask_is[0], None) if mask_fs.size == 0 else slice(None, mask_fs[0]+1)]
+        slices = [slice(mask_is[0], None) if mask_fs.size == 0 else slice(None, mask_fs[0] + 1)]
     else:
         slices = []
-    
+
     for sl in slices:
         if sl.start and sl.stop:
-            # linear interpolation 
-            flux_i[sl] = np.linspace(flux[sl.start-1], flux[sl.stop], sl.stop - sl.start + 2)[1:-1]
+            # linear interpolation
+            flux_i[sl] = np.linspace(flux[sl.start - 1], flux[sl.stop], sl.stop - sl.start + 2)[1:-1]
         else:
-            flux_i[sl] = flux[sl.start-1] if sl.start else flux[sl.stop]
+            flux_i[sl] = flux[sl.start - 1] if sl.start else flux[sl.stop]
 
     return flux_i
 
 
 def mask_interp(
-    spectrum: np.ndarray[float], 
+    spectrum: np.ndarray[float],
     mask: np.ndarray[bool] | list[tuple[float, float]]
-    ) -> np.ndarray[float]:
+) -> np.ndarray[float]:
     """
     Linearly interpolate flux from edges of given mask.
     Masked edges are set to the nearest unmasked value.
@@ -172,7 +173,7 @@ def mask_interp(
     Returns:
     spectrum_i: np.ndarray
         Spectrum with interpolation over masked regions.
-    
+
     """
 
     if isinstance(mask, np.ndarray):
