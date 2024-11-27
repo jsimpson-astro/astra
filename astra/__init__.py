15779341e7da5d3482213cb473648d7b7ba18500
Jordan <jordansimpson613@gmail.com>
2024-11-27 14:31:36 +0100

    packaging

--- astra/__init__.py
+++ astra/__init__.py
@@ -1,3 +1,7 @@
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__package__ or __name__)
+
 import astra.correlate as correlate
 import astra.fitting as fitting
 import astra.integrate as integrate
