67cbe8dec534fe71713586852081e8ab1a692193
Jordan <jordansimpson613@gmail.com>
2025-07-01 15:25:57 +0200

    add deprecated import warning

--- astra/molly/__init__.py
+++ astra/molly/__init__.py
@@ -1,1 +1,7 @@
 from .analysis import do_xcor, do_optsub, do_ews
+from astra.utils._helpers import deprecated_import
+
+msg = (f"{__name__} is deprecated, and will be removed in a future version. "
+	"Equivalent and/or improved methods are available in astra.correlate and astra.fitting.")
+
+deprecated_import(msg)
