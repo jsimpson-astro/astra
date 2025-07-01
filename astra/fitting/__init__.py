5248dce296e050cde47aa8efb06c9f9b7f52eecb
Jordan <jordansimpson613@gmail.com>
2025-07-01 14:21:15 +0200

    matching.py deprecated

--- astra/fitting/__init__.py
+++ astra/fitting/__init__.py
@@ -1,3 +1,3 @@
-from .matching import optsub
+from .specfitting import optsub, SpectrumInterpolator, SpectrumFitter, UniformPrior, GaussianPrior
 from .rvcfitting import rvmc
 from .linefitting import linefitmc
