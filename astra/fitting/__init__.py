bd0776f78f982779d7ce3268183d2703ce4b9f74
Jordan <jordansimpson613@gmail.com>
2025-10-15 14:53:09 +0200

    adjust for specmodels and add __all__

--- astra/fitting/__init__.py
+++ astra/fitting/__init__.py
@@ -1,3 +1,22 @@
-from .specfitting import optsub, SpectrumInterpolator, SpectrumFitter, UniformPrior, GaussianPrior
+from .specfitting import optsub, SpectrumFitter
+from .specmodels import SpectrumInterpolator
 from .rvcfitting import rvmc
 from .linefitting import linefitmc
+from .core import UniformPrior, GaussianPrior
+import astra.fitting.specmodels as specmodels
+
+__all__ = [
+    'core',
+    'linefitting',
+    'matching',
+    'rvcfitting',
+    'specfitting',
+    'specmodels',
+    'optsub',
+    'SpectrumFitter',
+    'SpectrumInterpolator',
+    'rvmc',
+    'linefitmc',
+    'UniformPrior',
+    'GaussianPrior',
+]
