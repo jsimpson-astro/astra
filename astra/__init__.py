ef58bbd9ff0ea0b15fb768d2c1df69d339383521
Jordan <jordansimpson613@gmail.com>
2025-10-15 14:52:25 +0200

    lazy loading and clean up

--- astra/__init__.py
+++ astra/__init__.py
@@ -1,9 +1,51 @@
+__all__ = [
+    '__version__',
+    # '__bibtex__',
+    # subpackages (lazy-loaded?)
+    'correlate',
+    'fitting',
+    'integrate',
+    'plotting',
+    'utils'
+]
+
+# version
 import importlib.metadata
-
 __version__ = importlib.metadata.version(__package__ or __name__)
+# clean up namespace
+del importlib.metadata
 
-import astra.correlate as correlate
-import astra.fitting as fitting
-import astra.integrate as integrate
-import astra.plotting as plotting
-import astra.utils as utils
+# astropy import style
+def __getattr__(attr):
+    if attr in __all__:
+        from importlib import import_module
+
+        return import_module('astra.' + attr)
+
+    raise AttributeError(f"module 'astra' has no attribute {attr!r}")
+
+
+# redefine dir
+def __dir__():
+    return sorted(set(globals()).union(__all__))
+
+
+from types import ModuleType as __module_type__
+
+# clean up top-level namespace
+# delete everything not in __all__
+# or is a built-in attribute
+# or that isn't a submodule of this package
+for varname in dir():
+    if not (
+        (varname.startswith('__') and varname.endswith('__'))
+        or varname in __all__
+        or (
+            varname[0] != '_'
+            and isinstance(locals()[varname], __module_type__)
+            and locals()[varname].__name__.startswith(__name__ + '.')
+        )
+    ):
+        del locals()[varname]
+
+del varname, __module_type__
